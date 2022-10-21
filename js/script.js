15dd7e430c15dc862f60fa9b1a19d82f7295b76e
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2022-10-21 15:33:05 -0700

    Remove one previousElementSibling from if stament

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 const openButton  = document.querySelector(".open-button");
 const closeButton = document.querySelector(".close-button");
-console.log("Is the script running? Yes.")
+console.log("Is the script running? Yes.");
 
 if (openButton) {
-    console.log("Is the script statement running? Yes.")
+    console.log("Is the script statement running? Yes.");
     openButton.addEventListener("click", (event) => {
         event.target
         .parentElement
         .parentElement
         .parentElement
         .lastElementChild
-        .previousElementSibling
         .previousElementSibling.style.display = "flex";
     });
     closeButton.addEventListener("click", (event) => {
