52f8470480c8ddadf990c3a8dd4bd501bde1f3b4
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2022-10-21 14:52:07 -0700

    testing if script is running on live site

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const openButton  = document.querySelector(".open-button");
 const closeButton = document.querySelector(".close-button");
+console.log("Is the script running? Yes.")
 
 if (openButton) {
+    console.log("Is the script statement running? Yes.")
     openButton.addEventListener("click", (event) => {
         event.target
         .parentElement
