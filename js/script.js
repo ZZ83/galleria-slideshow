a83240fea3043db0594bebbe72d47b13eeba6d5b
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2024-04-09 20:57:51 -0700

    Fix modal bug

--- js/script.js
+++ js/script.js
@@ -1,18 +1,12 @@
-const openButton  = document.querySelector(".open-button");
+const openButton = document.querySelector(".open-button");
 const closeButton = document.querySelector(".close-button");
 
 if (openButton) {
     openButton.addEventListener("click", (event) => {
-        event.target
-        .parentElement
-        .parentElement
-        .parentElement
-        .lastElementChild
-        .previousElementSibling.style.display = "flex";
+        const modal = event.target.parentElement.parentElement.parentElement.querySelectorAll(".modal")[0];
+        modal.style.display = "flex";
     });
     closeButton.addEventListener("click", (event) => {
-        event.target
-        .parentElement
-        .parentElement.style.display = "none";
+        event.target.parentElement.parentElement.style.display = "none";
     });
 }
